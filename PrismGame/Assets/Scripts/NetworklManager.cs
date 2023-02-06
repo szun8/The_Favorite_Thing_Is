e33fa0bdbac78bce23b9792682016a0d831b3585
최시은 <chse0810@gmail.com>
2023-02-06 16:23:01 +0900

    NetworkManager
    with scripts

---
+++ PrismGame/Assets/Scripts/NetworklManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworklManager : MonoBehaviourPunCallbacks
+{
+
+    private void Awake()
+    {
+        Screen.SetResolution(1920, 1080, false);
+    }
+    
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void Update()
+    {
+        
+    }
+}
