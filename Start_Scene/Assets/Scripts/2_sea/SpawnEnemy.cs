a824f2b10732d2c670637b873ed14907581fd89b
최시은 <chse0810@gmail.com>
2023-03-27 13:11:49 +0900

    플레이어 수영 및 해파리 애니메이션 컨트롤러 적용 및 spawnManager.cs
    재인이 모델폴더에 메타데이터 추가된것도 올릴게요
    (아마 하이라키창에서 프리팹 만들고 하느라 생긴듯요)

--- Start_Scene/Assets/Scripts/2_sea/SpawnEnemy.cs
+++ Start_Scene/Assets/Scripts/2_sea/SpawnEnemy.cs
@@ -24,13 +24,18 @@ public class SpawnEnemy : MonoBehaviour
 
     Quaternion[] rotation;
 
-    public InfoFish[] FishA;    // 5마리
+    public InfoFish[] FishA;    
     public InfoFish[] Jelly;
     public InfoFish Boss;
+
+    public GameObject rangeJelly;  // 해파리가 랜덤 소환될 구역 오브젝트
+    BoxCollider rangeCollider;
 
     private void Awake()
     {
         rotation = new Quaternion[3];
+
+        rangeCollider = rangeJelly.GetComponent<BoxCollider>();
     }
 
     private void Start()
@@ -63,25 +68,42 @@ public class SpawnEnemy : MonoBehaviour
         switch (_type)
         {
             //case TypeFish.enemy:
-            //    _gameObject.speed = Random.Range(3f, 10f);
-            //    _gameObject.spawnSpot = new Vector3(Random.Range(-30f, 15f), Random.Range(-20f, -8f), Random.Range(-5, 25));
+            //    
             //    break;
             
             case TypeFish.boss:
                 _gameObject.speed = 20f;
-                _gameObject.spawnSpot = new Vector3(-75f, -5f, 10f);
+                _gameObject.spawnSpot = new Vector3(160f, 40f, 0f);
                 break;
 
             case TypeFish.jelly:
                 _gameObject.speed = 0f;
-                _gameObject.spawnSpot = new Vector3(Random.Range(-30f, 15f), Random.Range(-20f, -8f), Random.Range(-5, 25));
+                _gameObject.spawnSpot = Return_RandomPos_jelly();
                 break;
         }
 
         clone = Instantiate(enemy[(int)_type], _gameObject.spawnSpot, rotation[(int)_type]);
         clone.name = _gameObject.name;
-        clone.transform.parent = this.transform;
+        if (clone.tag == "Enemy") clone.transform.parent = this.transform;
+        else clone.transform.parent = GameObject.Find("Jelly_RespawnRange").transform;
 
         if (_type == TypeFish.boss) clone.gameObject.SetActive(false);
+    }
+
+    Vector3 Return_RandomPos_jelly()
+    {   // 지정한 특정구역 내에 해파리를 랜덤 소환해주는 함수
+        Vector3 originPos = rangeJelly.transform.position;
+        // 콜라이더의 사이즈를 가져오는 bound.size 사용
+        float range_X = rangeCollider.bounds.size.x;    // 세로
+        float range_Y = rangeCollider.bounds.size.y;    // 높이
+        float range_Z = rangeCollider.bounds.size.z;    // 가로
+
+        range_X = Random.Range((range_X / 2) * -1, range_X / 2);
+        range_Y = Random.Range(5f , range_Y-5f);
+        range_Z = Random.Range((range_Z / 2) * -1, range_Z / 2);
+        Vector3 RandomPos = new Vector3(range_X, range_Y, range_Z);
+
+        Vector3 respawnPos = originPos + RandomPos;
+        return respawnPos;
     }
 }
