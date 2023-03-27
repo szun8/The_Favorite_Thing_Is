e06752b029d53f1c705cb1a5867c921d1603ee94
최시은 <chse0810@gmail.com>
2023-03-28 05:31:48 +0900

    🦭 Scene_2_Sea
    플레이어 무브 스크립트를 제외한 나머지 변경 사항 업데이트
    - 새로 준 플레이어 walk anims 적용
    - 안쓰는 스크립트 삭제 및 씨네머신을 위한 스크립트 및 패키지

---
+++ Start_Scene/Assets/Scripts/2_sea/ControlVCam.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ControlVCam : MonoBehaviour
+{
+    public CinemachineVirtualCamera vc;
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+    }
+}
