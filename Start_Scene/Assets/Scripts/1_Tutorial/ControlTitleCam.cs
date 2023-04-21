8a6106419868c930a0f06776b2c06a871ca56df4
최시은 <chse0810@gmail.com>
2023-04-22 04:09:51 +0900

    많은 걸 해서요,,,설명란 읽어주세요!
    1. 튜토리얼
    - 자막(UIManager.cs수정) 및 씨네머신 기능
    - 스카이박스 설정, 머티리얼 및 배경느낌 개선
    - UI asset png clip
    - butterfly particle 적용했는데 관련 에셋 아직 푸쉬 안함
    2. 심해씬
    - 전체 오브젝트 이상한 회전값 수정 및 풍성한 환경 설정
    - 보스 추적기능 및 그에 따른 플레이어 스크립트 수정
    3-4. 거울룸
    - 거울룸 플레이어 및 씨네머신 생성 및 스크립트 적용
    // 네트워크 매니저 아직 푸쉬 안함

---
+++ Start_Scene/Assets/Scripts/1_Tutorial/ControlTitleCam.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ControlTitleCam : MonoBehaviour
+{
+    CinemachineVirtualCamera titleCam;
+    public Vector3 followOffset, trackedOffset;
+    Vector3 originFollowOffset, originTrackedOffset;
+
+    bool isCam = false, isOrigin;
+    void Start()
+    {
+        titleCam = GetComponent<CinemachineVirtualCamera>();
+        originFollowOffset = titleCam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset;
+        originTrackedOffset = titleCam.GetCinemachineComponent<CinemachineComposer>().m_TrackedObjectOffset;
+    }
+
+    private void Update()
+    {
+        if (isCam)
+        {
+            titleCam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset
+                = Vector3.Lerp(titleCam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset, followOffset, Time.deltaTime * 2f);
+            titleCam.GetCinemachineComponent<CinemachineComposer>().m_TrackedObjectOffset
+                = Vector3.Lerp(titleCam.GetCinemachineComponent<CinemachineComposer>().m_TrackedObjectOffset, trackedOffset, Time.deltaTime * 2f);
+        }
+        else if (isOrigin)
+        {
+            titleCam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset
+                = Vector3.Lerp(titleCam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset, originFollowOffset, Time.deltaTime * 2f);
+            titleCam.GetCinemachineComponent<CinemachineComposer>().m_TrackedObjectOffset
+                = Vector3.Lerp(titleCam.GetCinemachineComponent<CinemachineComposer>().m_TrackedObjectOffset, originTrackedOffset, Time.deltaTime * 2f);
+        }
+    }
+
+    public void StartTitle()
+    {
+        StartCoroutine(ControlTitle());
+    }
+
+    IEnumerator ControlTitle()
+    {
+        isCam = true;
+        yield return new WaitForSeconds(1f);
+
+        UIManager.instnace.RunAnims("isTitle");
+        yield return new WaitForSeconds(2.5f);
+
+        isCam = false;
+        isOrigin = true;
+        yield return new WaitForSeconds(2f);
+
+        isOrigin = false;
+    }
+}
---
+++ Start_Scene/Assets/Scripts/3_Cave/3-4_Mirror/InitCam.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Cinemachine;
+
+public class InitCam : MonoBehaviourPun
+{
+    CinemachineVirtualCamera vBack;
+
+    void Awake()
+    {
+        vBack = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    public void SetPlayerCam(int playerCnt)
+    {
+        Debug.Log(playerCnt);
+        if (playerCnt == 1)
+        {
+            vBack.Follow = GameObject.Find("MirrorPlayer_1").transform;
+            vBack.LookAt = GameObject.Find("MirrorPlayer_1").transform;
+        }
+        else if (playerCnt == 2)
+        {
+            vBack.Follow = GameObject.Find("MirrorPlayer_2").transform;
+            vBack.LookAt = GameObject.Find("MirrorPlayer_2").transform;
+        }
+    }
+}
