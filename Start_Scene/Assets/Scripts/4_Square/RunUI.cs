1950f72c3a5a96245eea7c7444f8442624fb89a4
최시은 <chse0810@gmail.com>
2023-04-27 12:59:38 +0900

    빌드 app 및 광장 UI 적용

---
+++ Start_Scene/Assets/Scripts/4_Square/RunUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RunUI : MonoBehaviourPun
+{
+    GameObject realPlayer;
+    void GetPhotonViewID()
+    {
+        GameObject[] player = GameObject.FindGameObjectsWithTag("Player");
+        foreach (var item in player)
+        {
+            if (item.GetPhotonView().IsMine) realPlayer = item;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        GetPhotonViewID();
+        if(other.gameObject.GetComponentInParent<PhotonView>().ViewID == realPlayer.GetPhotonView().ViewID)
+        {
+            if(gameObject.name.Contains("L_press"))
+                UIManager.instnace.RunAnims("isL_Square");
+        }
+    }
+}
