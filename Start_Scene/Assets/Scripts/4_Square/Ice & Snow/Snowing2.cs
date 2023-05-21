0a6435221b677e9999ee0993bf4d7712c43d840d
hohojlike <likebell99@naver.com>
2023-05-21 18:11:32 +0900

    Tutorial + B
    튜토리얼 (윈도우 기준) 점프력 8-> 10 조정
    B 백색광 자막 위치 넣어둠 ㅎㅎ

--- Start_Scene/Assets/Scripts/4_Square/Ice & Snow/Snowing2.cs
+++ Start_Scene/Assets/Scripts/4_Square/Ice & Snow/Snowing2.cs
@@ -10,11 +10,6 @@ public class Snowing2 : MonoBehaviourPun
     PhotonView PV;
 
     public bool isLight = false; //이 단상에서 플레이어가 빛내니?
-    public int playerCnt = 0; // 발판을 밟고 있는 플레이어 수
-
-    private GameObject Player; // 단상에 충돌한 플레이어 
-    private GameObject P2;
-
 
     //ray 검출용 
     private bool isPlayer = false;
@@ -45,72 +40,41 @@ public class Snowing2 : MonoBehaviourPun
         isPlayer = Physics.Raycast(transform.position + new Vector3(0, 1f, 0), Vector3.up, out player, 1.7f, LayerMask.GetMask("Player"));
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            playerCnt++;
-
-            if (playerCnt == 1) Player = collision.gameObject;
-            else if (playerCnt == 2) P2 = collision.gameObject;
-
-        }
-    }
 
     private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            playerCnt--;
-
-            if (P2 == null) //1인용 단상 경우 
-            {
-                Player = null;
-
-                if (isSendOne && stairAnim.GetBool("isLight")) // 플레이어가 빛을 안끈 상태로 단상 탈출시 안꺼지는 문제 해결
-                {
-                    PV.RPC("SyncBlue", RpcTarget.AllBuffered, false);
-                    PV.RPC("SyncSnow", RpcTarget.AllBuffered, false);
-                    isSendOne = false;
-                }
-            }
-
-            else if (P2 != null) //1 2p 중 1p가 나간 경우 2p를 1p로 해줘야함 
-            {
-                if (collision.gameObject == Player)
-                {
-                    Player = P2;
-                    P2 = null;
-                }
-
-                else if (collision.gameObject == P2) P2 = null;
-            }
-
+           if(isSendOne && stairAnim.GetBool("isLight") && isLight)
+           {
+                PV.RPC("SyncBlue", RpcTarget.AllBuffered, false);
+                PV.RPC("SyncSnow", RpcTarget.AllBuffered, false);
+           }
         }
     }
 
     void CheckLight()
     {
-        if (isPlayer && player.collider.gameObject == Player)
+        if (isPlayer && player.collider != null)
         {
-            Debug.Log("1p입징 ");
-            if (Player.GetComponent<MultiPlayerMove>().b_pressed && !stairAnim.GetBool("isLight"))
+            if (player.collider.gameObject.GetComponent<MultiPlayerMove>().b_pressed && !stairAnim.GetBool("isLight"))
             {
                 PV.RPC("SyncBlue", RpcTarget.AllBuffered, true);
                 PV.RPC("SyncSnow", RpcTarget.AllBuffered, true);
-                //isSendOne = true;
-                /*if (!isLight) //단상을 최초로 킨 경우
-                {
+                    //isSendOne = true;
+                    /*if (!isLight) //단상을 최초로 킨 경우
+                    {
                     
-                }*/
+                    }*/
             }
 
             else if (isSendOne && stairAnim.GetBool("isLight")) //켰다가 레이 위에서 꺼져있을때
             {
                 PV.RPC("SyncBlue", RpcTarget.AllBuffered, false);
                 PV.RPC("SyncSnow", RpcTarget.AllBuffered, false);
-                //isSendOne = false;
+                    //isSendOne = false;
             }
+            
         }
         else if (isSendOne && stairAnim.GetBool("isLight")) //단상 레이 아예 나가버린 경우 
         {
@@ -131,7 +95,5 @@ public class Snowing2 : MonoBehaviourPun
     void SyncSnow(bool value)
     {
         stairAnim.SetBool("isLight", value);
-
-        
     }
 }
--- Start_Scene/Assets/Scripts/4_Square/RunUI.cs
+++ Start_Scene/Assets/Scripts/4_Square/RunUI.cs
@@ -23,15 +23,17 @@ public class RunUI : MonoBehaviourPun
             if(gameObject.name.Contains("L_press"))
                 UIManager.instnace.RunAnims("isL_Square");
 
+            if(gameObject.name.Contains("Get") && !other.gameObject.GetComponentInParent<MultiPlayerMove>().getRed)
+                UIManager.instnace.RunAnims("isL_Square");
+
             if(gameObject.name == "press R" && other.gameObject.GetComponentInParent<MultiPlayerMove>().getRed)
                 UIManager.instnace.RunAnims("isLight_R");
 
-            if (gameObject.name == "press G1" && other.gameObject.GetComponentInParent<MultiPlayerMove>().getGreen)
-                UIManager.instnace.RunAnims("isLight_G");
-
-            if (gameObject.name == "press G2" && other.gameObject.GetComponentInParent<MultiPlayerMove>().getGreen)
+            if (gameObject.name.Contains("G") && other.gameObject.GetComponentInParent<MultiPlayerMove>().getGreen)
                 UIManager.instnace.RunAnims("isLight_G"); 
-            
+
+            if (gameObject.name.Contains("B") && other.gameObject.GetComponentInParent<MultiPlayerMove>().getGreen)
+                UIManager.instnace.RunAnims("isLight_B"); 
         }
     }
 }
