d62ee7c7607a96ddab9097949f3f895234b7bf0f
hohojlike <likebell99@naver.com>
2023-05-22 17:47:12 +0900

    B 기능 완
    ~~~~

--- Start_Scene/Assets/Scripts/4_Square/Ice & Snow/Snowing2.cs
+++ Start_Scene/Assets/Scripts/4_Square/Ice & Snow/Snowing2.cs
@@ -6,10 +6,14 @@ using Photon.Pun;
 public class Snowing2 : MonoBehaviourPun
 {
     public Animator stairAnim; //계단 애니메이션 
-
+    public Snowing2 anotherPlate;
     PhotonView PV;
 
     public bool isLight = false; //이 단상에서 플레이어가 빛내니?
+    public int playerCnt =0;
+
+    GameObject Player;
+    GameObject P2;
 
     //ray 검출용 
     private bool isPlayer = false;
@@ -17,18 +21,14 @@ public class Snowing2 : MonoBehaviourPun
 
     private bool isSendOne = false; //패킷 반복 수 줄이기
 
-    void Awake()
-    {
-        PV = GetComponent<PhotonView>();
-    }
+    void Awake() => PV = GetComponent<PhotonView>();
 
-    // Update is called once per frame
     void Update()
     {
         Ray();
 
         if (PhotonNetwork.InRoom)
-        {
+        { 
             CheckLight(); //플레이어가 단상 레이에 검출 될때 키고 안키고
         }
         
@@ -41,46 +41,75 @@ public class Snowing2 : MonoBehaviourPun
     }
 
 
+    private void OnCollisionEnter(Collision collision)
+    { 
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerCnt++;
+            if (playerCnt == 1) Player = collision.gameObject;
+            else if (playerCnt == 2) P2 = collision.gameObject;
+
+        }
+    }
+
     private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-           if(isSendOne && stairAnim.GetBool("isLight") && isLight)
+           playerCnt--;
+
+           if (P2 == null) //1인용 단상 경우 
            {
-                PV.RPC("SyncBlue", RpcTarget.AllBuffered, false);
-                PV.RPC("SyncSnow", RpcTarget.AllBuffered, false);
+               Player = null;
+               if (isSendOne && isLight && !anotherPlate.isLight) // 내 불은 켜져있고, 상대 불은 꺼져 있는 경우만 exit시 
+               {
+                   PV.RPC("SyncBlue", RpcTarget.AllBuffered, false);
+                   PV.RPC("SyncSnow", RpcTarget.AllBuffered, false);
+               }
+
            }
+
+           else if (P2 != null) //1 2p 중 1p가 나간 경우 2p를 1p로 해줘야함 
+           {
+               if (collision.gameObject == Player)
+               {
+                   Player = P2;
+                   P2 = null;
+               }
+
+               else if (collision.gameObject == P2) P2 = null;
+           }
+           
         }
     }
 
     void CheckLight()
     {
-        if (isPlayer && player.collider != null)
+        //ray 검출 되고 + 검출 된 놈이 먼저 들어온 놈이어야 함 
+        if (isPlayer && player.collider != null && Player != null)
         {
-            if (player.collider.gameObject.GetComponent<MultiPlayerMove>().b_pressed && !stairAnim.GetBool("isLight"))
+            if(player.collider.transform.parent.gameObject == Player)
             {
-                PV.RPC("SyncBlue", RpcTarget.AllBuffered, true);
-                PV.RPC("SyncSnow", RpcTarget.AllBuffered, true);
-                    //isSendOne = true;
-                    /*if (!isLight) //단상을 최초로 킨 경우
-                    {
-                    
-                    }*/
-            }
+                //내가 b를 키고 상대 불은 꺼져있는 경우
+                if (player.collider.gameObject.GetComponentInParent<MultiPlayerMove>().b_pressed && !anotherPlate.isLight)
+                {
+                    PV.RPC("SyncBlue", RpcTarget.AllBuffered, true);
+                    PV.RPC("SyncSnow", RpcTarget.AllBuffered, true);
+                }
 
-            else if (isSendOne && stairAnim.GetBool("isLight")) //켰다가 레이 위에서 꺼져있을때
-            {
-                PV.RPC("SyncBlue", RpcTarget.AllBuffered, false);
-                PV.RPC("SyncSnow", RpcTarget.AllBuffered, false);
-                    //isSendOne = false;
+                //내가 레이에서 상대가 불 꺼져 있을 때에 내가 불 안키고 있으면 불 꺼짐
+                else if (isSendOne && !anotherPlate.isLight) //켰다가 레이 위에서 꺼져있을때
+                {
+                    PV.RPC("SyncBlue", RpcTarget.AllBuffered, false);
+                    PV.RPC("SyncSnow", RpcTarget.AllBuffered, false);
+                }
             }
             
         }
-        else if (isSendOne && stairAnim.GetBool("isLight")) //단상 레이 아예 나가버린 경우 
+        else if (Player !=null && isSendOne && !anotherPlate.isLight) //단상 레이 아예 나가버린 경우 
         {
             PV.RPC("SyncBlue", RpcTarget.AllBuffered, false);
             PV.RPC("SyncSnow", RpcTarget.AllBuffered, false);
-            //isSendOne = false;
         }
     }
 
